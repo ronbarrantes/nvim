380c04e4b78330522607a7ddff76eaa4b1a1bd8e
ronbarrantes <ronbarrantes@gmail.com>
2023-08-16 11:44:44 -0700

    Add harpoon

--- lua/ronb/packer.lua
+++ lua/ronb/packer.lua
@@ -15,11 +15,8 @@ return require('packer').startup(function(use)
 
   use { "bluz71/vim-moonfly-colors", as = "moonfly" }
 
-  -- use { 'nvim-treesitter/nvim-treesitter', { run = ":TSUpdate"} }
-  -- use ('nvim-treesitter/playground')
-
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'} )
-
   use( 'nvim-treesitter/playground' )
 
+  use('theprimeagen/harpoon')
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  harpoon = {
+    loaded = true,
+    path = "/Users/ronbarrantes/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
+  },
   moonfly = {
     loaded = true,
     path = "/Users/ronbarrantes/.local/share/nvim/site/pack/packer/start/moonfly",
