01166cf4094f52183639aaa33ac0cfe7eb3144b3
ronbarrantes <ronbarrantes@gmail.com>
2023-08-16 11:22:04 -0700

    Re-Add Playground

--- lua/ronb/packer.lua
+++ lua/ronb/packer.lua
@@ -20,5 +20,6 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'} )
 
+  use( 'nvim-treesitter/playground' )
 
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,6 +89,11 @@ _G.packer_plugins = {
     path = "/Users/ronbarrantes/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
+  playground = {
+    loaded = true,
+    path = "/Users/ronbarrantes/.local/share/nvim/site/pack/packer/start/playground",
+    url = "https://github.com/nvim-treesitter/playground"
+  },
   ["plenary.nvim"] = {
     loaded = true,
     path = "/Users/ronbarrantes/.local/share/nvim/site/pack/packer/start/plenary.nvim",
