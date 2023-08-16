7e91ae4455b7a0e8377be71ea723a1887deb380a
Ron Barrantes <ronbarrantes@gmail.com>
2023-08-16 12:24:31 -0700

    Add extra packages to the nvim config (#1)
    * Add harpoon
    
    * Update harpoon file
    
    * Add undotree
    
    * Add fugitive

--- lua/ronb/packer.lua
+++ lua/ronb/packer.lua
@@ -15,11 +15,12 @@ return require('packer').startup(function(use)
 
   use { "bluz71/vim-moonfly-colors", as = "moonfly" }
 
-  -- use { 'nvim-treesitter/nvim-treesitter', { run = ":TSUpdate"} }
-  -- use ('nvim-treesitter/playground')
-
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'} )
-
   use( 'nvim-treesitter/playground' )
 
+  use('theprimeagen/harpoon')
+
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
 end)
