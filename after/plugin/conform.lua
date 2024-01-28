fffdccd13acc2f418e78e72710f5c1c05e24ae39
ronbarrantes <ronbarrantes@gmail.com>
2024-01-28 15:42:29 -0800

    Update conform.lua

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -5,7 +5,41 @@ require("conform").setup({
     python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
+
+    go = { "goimports", "gofmt" },
+
+    -- Use the "*" filetype to run formatters on all filetypes.
+    ["*"] = { "codespell" },
+    -- Use the "_" filetype to run formatters on filetypes that don't
+    -- have other formatters configured.
+    ["_"] = { "trim_whitespace" },
+
+
   },
+
+
+  -- If this is set, Conform will run the formatter on save.
+  -- It will pass the table to conform.format().
+  -- This can also be a function that returns the table.
+  format_on_save = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_fallback = true,
+    timeout_ms = 500,
+  },
+  -- If this is set, Conform will run the formatter asynchronously after save.
+  -- It will pass the table to conform.format().
+  -- This can also be a function that returns the table.
+  format_after_save = {
+    lsp_fallback = true,
+  },
+
+  formatters = {
+    -- If the formatter supports range formatting, create the range arguments here
+
+    range_args = function(ctx)
+      return { "--line-start", ctx.range.start[1], "--line-end", ctx.range["end"][1] }
+    end,
+  }
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
