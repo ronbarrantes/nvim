d2667f95d713492969da14ca5a7ee06f57195658
ronbarrantes <ronbarrantes@gmail.com>
2024-04-10 01:22:32 -0700

    Add a bunch of stuff so far

--- lua/ronb/core/options.lua
+++ lua/ronb/core/options.lua
@@ -1,4 +1,4 @@
-vim.cmd("let g:netrw_liststyle = 3")
+-- vim.cmd("let g:netrw_liststyle = 3")
 vim.o.mouse = "" -- remove mouse support
 
 local opt = vim.opt
---
+++ lua/ronb/plugins/init.lua
@@ -0,0 +1,5 @@
+return {
+  "nvim-lua/plenary.nvim", -- use by many plugins
+  "christoomey/vim-tmux-navigator", -- for tmux & split windows navigation }
+}
+
