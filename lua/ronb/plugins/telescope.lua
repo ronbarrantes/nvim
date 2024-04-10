618d41b49784d855e4475d121b2edc9e322caf3d
ronbarrantes <ronbarrantes@gmail.com>
2024-04-10 02:20:33 -0700

    More telescope

--- lua/ronb/plugins/telescope.lua
+++ lua/ronb/plugins/telescope.lua
@@ -1,46 +1,50 @@
-
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  {"nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  {"nvim-tree/nvim-web-devicons"},
-  config = function() 
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
     telescope.setup({
-        defaults = {
-        file_ignore_patterns = {
-          "node_modules"
-        },
+      defaults = {
+        path_display = { "smart" },
         mappings = {
           i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          }
-        }
+          },
+        },
       },
-      pickers = {
-      },
-      extensions = {
-      }
     })
 
     telescope.load_extension("fzf")
-    local keymap = vim.keymap
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc="Find in files"})
 
-
-
-
-  end
-}
--- local builtin = require('telescope.builtin')
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+ local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find in file" })
--- vim.keymap.set('n', '<C-f>', builtin.git_files, { desc = "Find in git files" })
--- vim.keymap.set('n', '<leader>fs', function()
---   builtin.grep_string({ search = vim.fn.input("Grep > ") })
--- end, { desc = "Find string"}
--- )
+    keymap.set('n', '<leader>fs', 
+      function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end, 
+      { desc = "Find string" }
+     )
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fl", builtin.live_grep, { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+    keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Find in git files" })
+  end,
+}
+
+
+
+
+
 
