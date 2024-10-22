f73261b5fe974229b6c3d672458613a942a86c9d
ronbarrantes <ronbarrantes@gmail.com>
2024-10-22 17:07:55 -0600

    Add harpoon

---
+++ lua/ronb/plugins/harpoon.lua
@@ -0,0 +1,7 @@
+return {
+	"ThePrimeagen/harpoon",
+	keys = { -- load the plugin only when using it's keybinding:
+		-- { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		--k		{ "<leader>u", vim.cmd.UndotreeToggle },
+	},
+}
