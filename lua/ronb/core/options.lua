eba0980c4b8c23b43a5045fb9cd156713ffff42b
Ron Barrantes <ronbarrantes@gmail.com>
2025-02-17 00:31:53 -0600

    Updates

--- lua/ronb/core/options.lua
+++ lua/ronb/core/options.lua
@@ -10,24 +10,23 @@ opt.mouse = ""
 opt.eol = true
 
 -- Ensure end of line character at the end of files
-opt.fixendofline = true
+vim.opt.fixendofline = true
 
 -- Set file format to Unix
 opt.fileformat = "unix"
 
--- -- Autocmd to ensure newline at end of file
+-- Autocmd to ensure newline at end of file
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 -- 	pattern = "*",
 -- 	command = [[if &fixendofline | call setline('$', substitute(getline('$'), '\n\+$', '', '')) | endif]],
 -- })
---
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function()
-		vim.opt.fixendofline = true
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		vim.opt.fixendofline = true
+-- 	end,
+-- })
 
 opt.relativenumber = true
 opt.number = true
