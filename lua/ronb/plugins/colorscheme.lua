997592c01fa86e0baa35fbbd6bffaf7221325376
Ron Barrantes <ronbarrantes@gmail.com>
2025-03-17 20:07:51 -0400

    Add color scheme

--- lua/ronb/plugins/colorscheme.lua
+++ lua/ronb/plugins/colorscheme.lua
@@ -2,19 +2,32 @@ return {
 	"folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
-		local bg = "#030303" -- "#010618"
-		local bg_dark = "#010413"
-		local bg_highlight = "#042642"
-		local bg_search = "#0A65AC"
-		local bg_visual = "#274378"
-		local fg = "#CBD0E0"
-		local fg_dark = "#B4C0D9"
-		local fg_gutter = "#627E97"
-		local border = "#547998"
+		local function get_system_theme()
+			local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+			if handle == nil then
+				return "light" -- Default to light theme if handle is nil
+			end
+			local result = handle:read("*a")
+			handle:close()
+			if result == nil then
+				return "light" -- Default to light theme if result is nil
+			end
+			return result:match("Dark") and "dark" or "light"
+		end
+
+		local theme = get_system_theme()
+		local bg = theme == "dark" and "#030303" or "#FFFFFF"
+		local bg_dark = theme == "dark" and "#010413" or "#F0F0F0"
+		local bg_highlight = theme == "dark" and "#042642" or "#E0E0E0"
+		local bg_search = theme == "dark" and "#0A65AC" or "#D0D0D0"
+		local bg_visual = theme == "dark" and "#274378" or "#C0C0C0"
+		local fg = theme == "dark" and "#CBD0E0" or "#000000"
+		local fg_dark = theme == "dark" and "#B4C0D9" or "#333333"
+		local fg_gutter = theme == "dark" and "#627E97" or "#666666"
+		local border = theme == "dark" and "#547998" or "#999999"
 
 		require("tokyonight").setup({
-			style = "night",
-
+			style = theme == "dark" and "night" or "day",
 			on_colors = function(colors)
 				colors.bg = bg
 				colors.bg_dark = bg_dark
