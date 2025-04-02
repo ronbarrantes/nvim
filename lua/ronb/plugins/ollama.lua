77fe5765622cd208b29e2f9773fa3de090f1fcff
Ron Barrantes <ronbarrantes@gmail.com>
2025-04-02 01:12:43 -0400

    Modify streamer mode

--- lua/ronb/plugins/ollama.lua
+++ lua/ronb/plugins/ollama.lua
@@ -2,7 +2,6 @@
 local OllamaConfig = {}
 
 return {
-
 	"nomnivore/ollama.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
--- lua/ronb/plugins/streamer-mode.lua
+++ lua/ronb/plugins/streamer-mode.lua
@@ -1,1 +1,31 @@
-return { 'Kolkhis/streamer-mode.nvim' }
+return {
+	"Kolkhis/streamer-mode.nvim",
+	-- config = function()
+	--   require("streamer-mode").setup({
+	--     -- Use the default paths and keywords in addition to your own.
+	--     paths = {
+	--       -- While working in buffers that match any path or filetype listed here,
+	--       -- streamer-mode will conceal all keywords in the `keywords` table.
+	--       "*/dotenv/*",
+	--       "*/.env*",
+	--       "*.c",
+	--       "~/.bash*",
+	--       "~/.dotfiles/*",
+	--       "~/.my_config/*",
+	--       "*/.gitconfig",
+	--     },
+	--     keywords = {
+	--       "token",
+	--       "auth_token",
+	--       "auth_key",
+	--       "my_key",
+	--       "MySecretVariable",
+	--       "MyAddress",
+	--     },
+	--     level = "edit", -- | 'secure' | 'soft'
+	--     default_state = "on", -- | 'off'
+	--     conceal_char = "X", -- Can be any character
+	--   })
+	-- end,
+}
+
