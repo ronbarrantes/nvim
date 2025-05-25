d88a5d4d98a861ab9397374eda28d16e0590f332
Ron Barrantes <ronbarrantes@gmail.com>
2025-05-25 17:17:10 -0400

    Add workaround

--- lua/ronb/plugins/lsp/mason.lua
+++ lua/ronb/plugins/lsp/mason.lua
@@ -1,38 +1,47 @@
 return {
 	"williamboman/mason.nvim",
-	opts = {
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
-			},
-		},
-	},
 	dependencies = {
-		{
-			"williamboman/mason-lspconfig.nvim",
-			opts = {
-				ensure_installed = {
-					-- "tsserver",
-					-- "html",
-					-- "htmx",
-					-- "jsonls",
-					-- "cssls",
-					-- "autotools_ls",
-					-- "tailwindcss",
-					-- "asm_lsp",
-					-- "gopls",
-					-- "golangci_lint_ls",
-					-- "graphql",
-					-- "emmet_ls",
-					-- "marksman",
-					-- "sqlls",
-					-- "prismals",
-					-- "pyright",
-				},
-				automatic_installation = true,
-			},
-		},
+		"williamboman/mason-lspconfig.nvim",
 	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				-- "tsserver",
+				-- "html",
+				-- "htmx",
+				-- "jsonls",
+				-- "cssls",
+				-- "autotools_ls",
+				-- "tailwindcss",
+				-- "asm_lsp",
+				-- "gopls",
+				-- "golangci_lint_ls",
+				-- "graphql",
+				-- "emmet_ls",
+				-- "marksman",
+				-- "sqlls",
+				-- "prismals",
+				-- "pyright",
+			},
+			automatic_installation = true,
+		})
+	end,
 }
